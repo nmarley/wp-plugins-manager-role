78acb300b71ef52bdef37b1a8300577a8443eaad
Nathan Marley <nathan.marley@gmail.com>
2015-08-21 07:21:25 -0500

    fix, and add plugin header

--- wp-plugins-manager-role.php
+++ wp-plugins-manager-role.php
@@ -1,4 +1,15 @@
 <?php
+/**
+ * Plugin Name: Plugin Manager Role
+ * Plugin URI: http://nathanmarley.com/wp-plugins-manager-role
+ * Description: This plugin adds a user type ("role") which allows them to only manage plugins.
+ * Version: 0.0.1
+ * Author: Nathan Marley
+ * Author URI: http://nathanmarley.com
+ * License: GPL2
+ */
+
+  defined( 'ABSPATH' ) or die( 'No script kiddies please!' );
 
   $role_internal_name = 'plugin_manager';
   $role_display_name = 'Plugin Manager';
@@ -6,11 +17,12 @@
   # TODO: enable an admin screen which allows to enable/disable each one of these,
   #       or maybe on a per-user basis
   $plugin_manager_roles = array(
-    'activate_plugins' => true
-    'edit_plugins' => true
-    'install_plugins' => true
-    'update_plugins' => true
-    'delete_plugins' => true
+    'activate_plugins' => true,
+    'edit_plugins' => true,
+    'install_plugins' => true,
+    'update_plugins' => true,
+    'delete_plugins' => true,
   );
 
-  add_role( $role_name, $display_name, $plugin_manager_roles );
+  add_role( $role_internal_name, $role_display_name, $plugin_manager_roles );
+
